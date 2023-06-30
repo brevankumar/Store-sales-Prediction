edac1d686ffccdfa5980bb6765fb9c9b04f6d372
brevankumar <brevankumar@gmail.com>
2023-06-30 22:50:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Store Sales Prediction
+
+### Create an Environment
+```
+conda create -p sample python==3.9.15
+
+conda activate sample/
+```
+
+### Install necessary libraries
+```
+pip install  -r requirements.txt
+```
+
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+      
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+    return requirements
+        
+
+
+setup(
+    name="Store Sales Prediction",
+    version="0.0.1",
+    author="Revan",
+    author_email="brevankumar@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+      )
